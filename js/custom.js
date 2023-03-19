e48516f59b94838c1f48a332a30afd67b6cf09c6
orabipro <30470980+orabipro@users.noreply.github.com>
2023-03-19 13:55:28 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,36 @@
+(function ($) {
+
+  "use strict";
+
+    // PRE LOADER
+    $(window).load(function(){
+      $('.preloader').fadeOut(1000); // set duration in brackets    
+    });
+
+
+    //Navigation Section
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+
+    // PARALLAX EFFECT
+    $.stellar();  
+
+
+    // SMOOTHSCROLL
+    $(function() {
+      $('.navbar-default a, #home a, footer a').on('click', function(event) {
+        var $anchor = $(this);
+          $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+          }, 1000);
+            event.preventDefault();
+      });
+    });  
+
+
+    // WOW ANIMATION
+    new WOW({ mobile: false }).init();
+
+})(jQuery);
